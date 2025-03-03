ec0d43aea3f83b0f1ef221226038d14aa1e8ec50
mukut-d <mukut.m.das2000@gmail.com>
2025-03-03 18:06:32 +0530

    worked on landing page image

--- client/src/components/Extra/Extra.jsx
+++ client/src/components/Extra/Extra.jsx
@@ -1,8 +1,24 @@
+import "./Extra.css";
+
 const Extra = () => {
   return (
     <section className="extra-wrapper">
-      <div className="innerWidth extra-container padding">
-        
+      <div className="innerWidth flexCenter extra-container padding">
+        <div className="extra-left">
+          <div className="extra-title">
+            <h1>
+              Discover <br /> Most Perfect <br /> Property
+            </h1>
+          </div>
+        </div>
+        <div className="extra-right flexCenter">
+          <div className="image-container">
+            <img
+              src="../../../public/assets/images/building.jpg"
+              alt="building"
+            />
+          </div>
+        </div>
       </div>
     </section>
   );
