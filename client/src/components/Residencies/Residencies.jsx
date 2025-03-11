8e08bb9c3653f9a438d0ee68d86cb599db54152f
mukut-d <mukut.m.das2000@gmail.com>
2025-03-11 18:16:05 +0530

    made the slider button workable

--- client/src/components/Residencies/Residencies.jsx
+++ client/src/components/Residencies/Residencies.jsx
@@ -4,6 +4,16 @@ import "./Residencies.css";
 import data from "../../utilities/slider.json";
 import { sliderSettings } from "../../utilities/common";
 
+export const SliderButtons = ({}) => {
+  const swiper = useSwiper();
+  return (
+    <div className=" flexCenter r-button">
+      <button onClick={() => swiper.slidePrev()}>&lt;</button>
+      <button onClick={() => swiper.slideNext()}>&gt;</button>
+    </div>
+  );
+};
+
 const Residencies = () => {
   return (
     <section className="r-wrapper">
@@ -14,20 +24,23 @@ const Residencies = () => {
         </div>
 
         <Swiper {...sliderSettings}>
+          <SliderButtons />
           {data?.map((card, i) => {
             return (
-              <SwiperSlide key={i}>
-                <div className="flexColStart r-card">
-                  <img src={card.image} alt={card.name} />
-                  <span className="secondaryText r-price">
-                    <span style={{ color: "orange" }}>$ </span>
-                    <span>{card.price}</span>
-                  </span>
+              <>
+                <SwiperSlide key={i}>
+                  <div className="flexColStart r-card">
+                    <img src={card.image} alt={card.name} />
+                    <span className="secondaryText r-price">
+                      <span style={{ color: "orange" }}>$ </span>
+                      <span>{card.price}</span>
+                    </span>
 
-                  <span className="primaryText">{card.name}</span>
-                  <span className="secondaryText">{card.detail}</span>
-                </div>
-              </SwiperSlide>
+                    <span className="primaryText">{card.name}</span>
+                    <span className="secondaryText">{card.detail}</span>
+                  </div>
+                </SwiperSlide>
+              </>
             );
           })}
         </Swiper>
